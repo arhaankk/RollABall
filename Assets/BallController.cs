e115f628f37320c2c9ec884425ef92416d757fef
Arhaan <113068699+arhaankk@users.noreply.github.com>
2025-01-24 15:57:09 -0800

    Bonus Part 1
    completed bonus with no collision

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,6 +6,8 @@ public class InputManager : MonoBehaviour
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public Rigidbody sphereRigidbody;
     public float ballSpeed = 2f;
+    public int jumpHigh = 1;
+    public bool isGrounded = false;
     void Start()
     {
 
@@ -31,5 +33,24 @@ public class InputManager : MonoBehaviour
         }
         Vector3 inputXZPlane = new(inputVector.x, 0, inputVector.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded == true)
+            {
+                sphereRigidbody.linearVelocity = new Vector3(sphereRigidbody.linearVelocity.x, jumpHigh);
+                isGrounded = false;
+            }
+            
+        }
     }
+
+    private void OnCollision(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
 }
